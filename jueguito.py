6acd46f080f4c4cd4172292181f9da1f89bf12ff
ricardobeta545 <ricardobetafire545@gmail.com>
2026-05-23 00:24:43 -0500

    prueba1

---
+++ jueguito.py
@@ -0,0 +1,36 @@
+import random
+
+palabras = ["python", "capibara", "terror", "videojuego"]
+
+palabra = random.choice(palabras)
+letras_adivinadas = []
+vidas = 6
+
+while vidas > 0:
+    progreso = ""
+
+    for letra in palabra:
+        if letra in letras_adivinadas:
+            progreso += letra + " "
+        else:
+            progreso += "_ "
+
+    print("\nPalabra:", progreso)
+
+    if "_" not in progreso:
+        print("🎉 Ganaste")
+        break
+
+    intento = input("Ingresa una letra: ").lower()
+
+    if intento in palabra:
+        letras_adivinadas.append(intento)
+        print("✅ Correcto")
+    else:
+        vidas -= 1
+        print("❌ Incorrecto")
+        print("Vidas:", vidas)
+
+if vidas == 0:
+    print("💀 Perdiste")
+    print("La palabra era:", palabra)
